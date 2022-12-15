bdc35424a4ed4d23101fbf1022aaec8e6e26eeaf
Ilya <igromykhalin@bk.ru>
2022-12-16 00:26:07 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿int [] CreateArray(int size)
+{
+    int[] array = new int [size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input Value of {i+1} element:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for(int i =0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+Console.Write("Input Array size: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int [] myArray = CreateArray(length);
+ShowArray(myArray);
